a889f8db5f57dcfa19e6a224a86ff5012be03cc8
calebe vila nova <calebesouzavasconcelos@gmail.com>
2018-06-21 16:21:33 -0300

    Criando componentes e passando valores por property binding

---
+++ src/app/studant/studant.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Student} from './studant.model'
+
+@Component({
+  selector: 'app-studant',
+  templateUrl: './studant.component.html',
+  styleUrls: ['./studant.component.css']
+})
+export class StudantComponent implements OnInit {
+
+  @Input() studant: Student
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
